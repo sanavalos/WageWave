2441585dd94cb5762ea5c4b95cf6a7f0aa1a842a
sanavalos <santiago.avalos@outlook.com>
2024-01-18 21:07:49 -0300

    feat: version 1.0

---
+++ README.md
@@ -0,0 +1,61 @@
+<a id="readme-top"></a>
+
+<br />
+<div align="center">
+  <img src="WageWave.jpg" alt="WageWave" width="150" height="150">
+
+</div>
+
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-project">About The Extension</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#usage">Usage</a></li>
+      </ul>
+    </li>
+    <li><a href="#contact">Contact</a></li>
+  </ol>
+</details>
+
+## About The Project
+
+WageWave is a Visual Studio Code extension designed for freelancers and professionals to effortlessly track and visualize their earnings in real-time, providing a convenient  way to stay informed about their accumulated income during coding sessions.
+
+### Built With
+
+- ![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black)
+- ![Visual Studio Code](https://img.shields.io/badge/Visual_Studio_Code-0078D4?style=for-the-badge&logo=visual%20studio%20code&logoColor=white)
+- ![NPM](https://img.shields.io/badge/NPM-%23000000.svg?style=for-the-badge&logo=npm&logoColor=white)
+
+## Getting Started
+
+First of all you must download this repository from Github as ZIP or clone it.
+
+### Usage
+
+* Press `F5` to open a new window with your extension loaded.
+* Run your command from the command palette by pressing (`Ctrl+Shift+P` or `Cmd+Shift+P` on Mac) and typing `Start WageWave! `.
+* You are able to pause the extension from keep adding earnings by running command `Pause WageWave. `
+* In case you want to resume it, you can run the command `Resume WageWave! `
+
+
+## Make changes
+
+* You can relaunch the extension from the debug toolbar after changing code in `extension.js`.
+* You can also reload (`Ctrl+R` or `Cmd+R` on Mac) the VS Code window with your extension to load your changes.
+
+## Contact
+
+Santiago Germán Avalos - santiago.avalos@outlook.com
+
+Github repo: [WageWave](https://github.com/sanavalos)
+
+<p align="right"><a href="#readme-top">back to top</a></p>
---
+++ extension.js
@@ -0,0 +1,115 @@
+const vscode = require('vscode');
+
+let hourlyRate;
+let totalEarned = 0;
+let statusBarItem;
+let timerInterval;
+let isWageWavePaused = false;
+
+function activate(context) {
+
+  const startWageWave = async () => {
+    if (isWageWavePaused) {
+      vscode.window.showInformationMessage('WageWave is already paused. Resume it using "WageWave: Resume WageWave" command.');
+      return;
+    }
+
+    const rateInputOptions = {
+      prompt: 'Enter your hourly rate (USD)',
+      placeHolder: 'e.g., 25',
+      validateInput: (value) => {
+        const parsedValue = parseFloat(value);
+        if (isNaN(parsedValue) || parsedValue <= 0) {
+          return 'Please enter a valid positive number for the hourly rate.';
+        }
+        return null;
+      },
+    };
+
+    hourlyRate = await vscode.window.showInputBox(rateInputOptions);
+
+    const intervalInputOptions = {
+      prompt: 'Enter update interval in minutes',
+      placeHolder: 'e.g., 5',
+      validateInput: (value) => {
+        const parsedValue = parseInt(value);
+        if (isNaN(parsedValue) || parsedValue <= 0) {
+          return 'Please enter a valid positive integer for the update interval.';
+        }
+        return null;
+      },
+    };
+
+    const interval = await vscode.window.showInputBox(intervalInputOptions);
+    timerInterval = parseInt(interval) * 60 * 1000; 
+
+    vscode.window.showInformationMessage(`Hourly rate set to $${hourlyRate}. WageWave started with ${interval}-minute intervals.`);
+    updateEarnedMoney(); 
+
+    const intervalId = setInterval(updateEarnedMoney, timerInterval);
+
+    context.subscriptions.push({
+      dispose: () => clearInterval(intervalId),
+    });
+  };
+
+  const updateEarnedMoney = () => {
+    if (!hourlyRate || isWageWavePaused) {
+      return;
+    }
+    if (statusBarItem){
+     const earnedMoney = ((hourlyRate / 60 / 60 / 1000) * timerInterval).toFixed(2);
+
+     totalEarned += parseFloat(earnedMoney);
+ 
+     const notification = vscode.window.showInformationMessage(`Earned money: $${earnedMoney}`);
+ 
+     setTimeout(() => {
+       notification.dispose();
+     }, 5000);
+ 
+    }
+    updateStatusBar();
+  };
+
+  const updateStatusBar = () => {
+    if (!statusBarItem) {
+      statusBarItem = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+      context.subscriptions.push(statusBarItem);
+    }
+
+    statusBarItem.text = `💰 Total Earned: $${totalEarned.toFixed(2)}`;
+    statusBarItem.show();
+  };
+
+  const pauseWageWave = () => {
+    if (!isWageWavePaused) {
+      isWageWavePaused = true;
+      vscode.window.showInformationMessage('WageWave paused. Resume it using "WageWave: Resume WageWave" command.');
+    }
+  };
+
+  const resumeWageWave = () => {
+    if (isWageWavePaused) {
+      isWageWavePaused = false;
+      vscode.window.showInformationMessage('WageWave resumed.');
+    }
+  };
+
+  let disposable = vscode.commands.registerCommand('wagewave.startWageWave', startWageWave);
+  let pauseDisposable = vscode.commands.registerCommand('wagewave.pauseWageWave', pauseWageWave);
+  let resumeDisposable = vscode.commands.registerCommand('wagewave.resumeWageWave', resumeWageWave);
+
+  context.subscriptions.push(disposable, pauseDisposable, resumeDisposable);
+}
+
+function deactivate() {
+  if (statusBarItem) {
+    statusBarItem.dispose();
+  }
+}
+
+module.exports = {
+  activate,
+  deactivate,
+};
